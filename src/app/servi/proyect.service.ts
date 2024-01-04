178e0f77d61b81cde67e6f156fdbbea366054def
Omar Alejandro Lozada <alejandro01lozada@gmail.com>
2024-01-04 13:28:41 -0600

    listo

--- src/app/servi/proyect.service.ts
+++ src/app/servi/proyect.service.ts
@@ -2,14 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { projects } from '../Models/projects';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProyectService {
 
   url = "http://localhost:8080";
+  pro : projects[]=[];
 
   constructor(private http:HttpClient) { }
 
-  //getProyect():Observable<any>{}
+  public get(endpoint: string): Observable<any> {
+    const url = `${this.url}/${endpoint}`;
+    return this.http.get(url);
+  }
+  
 }
